5fb056a26c0ddb771f0a1de75851bfab83f5f044
Matin <mkomijani323@yahoo.com>
2025-06-01 21:27:00 +0330

    fix: fix navigation problem

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -6,13 +6,15 @@ import { useAuth } from "@/stores/authStore";
 import type { ComponentType, FC, JSX } from "react";
 
 export function withAuth<P extends JSX.IntrinsicAttributes>(WrappedComponent: ComponentType<P>): FC<P> {
-  return function ProtectedComponent(props: P) {
+    return function ProtectedComponent(props: P) {
     const router = useRouter();
     const isLoggedIn = useAuth((state) => state.isLoggedIn);
 
     useEffect(() => {
       if (!isLoggedIn) {
         router.push("/login");
+      }else{
+        router.push("/")
       }
     }, [isLoggedIn, router]);
 
